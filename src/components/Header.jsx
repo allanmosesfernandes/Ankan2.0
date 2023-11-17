edd39a1258cc69fe40924a9f50e761f80e531a85
Allan Fernandes <allan.fernandes@responsiblelife.co.uk>
2023-11-17 23:53:44 +0000

    Add scroll effect to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "gatsby";
 
 function Header() {
+	const [isScrolled, setIsScrolled] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const offset = window.scrollY;
+			setIsScrolled(offset > 50); // Set the scroll threshold as needed
+		};
+
+		window.addEventListener("scroll", handleScroll);
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 	return (
-		<header>
+		<header
+			className={
+				isScrolled
+					? "fixed top-0 left-0 right-0 z-10 bg-white shadow-md ease-in duration-300"
+					: ""
+			}
+		>
 			<nav className="md:p-5 p-3 flex flex-row items-center">
 				<Link to="/">
 					<h1 className="font-gothic lg:text-5xl text-3xl">

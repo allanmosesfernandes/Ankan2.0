1b59f90e8c58401bf1b9435ef184905c32f58969
Allan Fernandes <allan.fernandes@responsiblelife.co.uk>
2023-11-17 19:32:47 +0000

    Update image variable in SEO component

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -6,14 +6,14 @@ function SEO({ title, description, pathname, children, imageURL }) {
 	const {
 		title: defaultTitle,
 		description: defaultDescription,
-		image,
+		image: defaultImage,
 		siteUrl,
 	} = useSiteMetadata();
 
 	const seo = {
 		title: title || defaultTitle,
 		description: description || defaultDescription,
-		image: image || imageURL,
+		image: imageURL || defaultImage,
 		url: `${siteUrl}${pathname || ""}`,
 	};
 	return (
